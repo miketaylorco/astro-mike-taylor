652a06f1d5d4ee22eb1aec7bd56c9b5f1ca4eb31
Mike Taylor <hello@miketaylor.co>
2026-01-13 14:42:34 +0000

    Trying to get posts to show

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
-// @ts-check
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
-import sanity from '@sanity/astro';
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
-
+    plugins: [tailwindcss()],
+  },
   integrations: [
+    // 👇 update these lines
     sanity({
       projectId: "xgztagdf",
       dataset: "production",
